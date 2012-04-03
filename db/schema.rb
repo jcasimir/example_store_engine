f6077c9f5f83b66feb4cdd3e96e156b9df5cbfa1
Jeff Casimir <jeff@casimircreative.com>
2012-04-03 16:56:44 -0400

    Scaffolding a Product

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20120403204619) do
+
+  create_table "products", :force => true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.decimal  "price",       :precision => 12, :scale => 2
+    t.datetime "created_at",                                 :null => false
+    t.datetime "updated_at",                                 :null => false
+  end
 
 end
