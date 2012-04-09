2d9e9dec6738ec9fb035ec4ccab200fea0772589
Jeff Casimir <jeff@casimircreative.com>
2012-04-09 15:17:29 -0400

    Remove outdated comment

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -21,13 +21,6 @@ class Cart < ActiveRecord::Base
     items.sum{|i| i.total }
   end
 
-  # Eventually, we'll want to use CartItems instead of
-  # using Products directly, because we will have more
-  # functionality to put onto CartItem (such as remembering)
-  # the price of a product at the time it was added to the cart).
-  #
-  # For now, we've may have looked too far ahead,
-  # because we do not yet need those features.
   def add_item(product)
     products << product
   end
